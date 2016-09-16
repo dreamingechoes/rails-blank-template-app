bdd95e8b0058d5d5a9ba744e9d9ba18fb8c781b7
Ivan Gonzalez <ivan@simplelogica.net>
2016-09-16 21:47:22 +0200

    Added before_action method on application_controller for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   layout :layout_by_resource
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def layout_by_resource
+  protected def layout_by_resource
     if devise_controller?
       "devise"
     else
       "application"
     end
   end
-  protected :layout_by_resource
+
+ protected def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+ end
 end
